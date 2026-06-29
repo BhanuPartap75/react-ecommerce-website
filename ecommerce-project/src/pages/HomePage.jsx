b732ba83b70845ae74c20c846d99c16044f41d37
BhanuPartap75 <bhanupratap9372@gmail.com>
2026-06-29 07:29:03 +0530

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import { Header } from "../components/Header";
 import { products } from "../../starting code/data/products";
 import CheckmarkIcon from "../assets/images/icons/checkmark.png";
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+   .then((response)=>{
+     response.json().then((data)=>{
+       console.log(data);
+     })
+   })
+
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
